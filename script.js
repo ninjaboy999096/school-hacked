046b6a99e8803ad62b653c2c5fdb50008e7548cc
ninjaboy999096 <129236203+ninjaboy999096@users.noreply.github.com>
2025-05-14 15:25:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://raw.githubusercontent.com/ninjaboy999096/school-hacked/refs/heads/main/yeah.html")
+fetch("https://raw.githubusercontent.com/ninjaboy999096/school-hacked/main/yeah.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
